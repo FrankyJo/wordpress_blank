ba7448c0bad062100af39bc95c27be25702ac5df
Denys Popov <sined007@gmail.com>
2023-01-24 18:02:31 +0200

    add script for critical CSS

--- customTheme/app/front/addStyles.php
+++ customTheme/app/front/addStyles.php
@@ -14,3 +14,62 @@ function addHeaderStyles()
 function addFooterStyles()
 {
 }
+
+/*
+* SHOW CRITICAL CSS
+*/
+function loadCriticalCss()
+{
+    global $post;
+
+    $page_template = get_page_template_slug($post->ID);
+
+    if ($page_template) {
+
+        $pattern = '/^templates\/page-(.*).php$/';
+        preg_match($pattern, $page_template, $result);
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-' . $result[1] . '.css');
+    }
+
+    if (get_archive_post_type()) {
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-' . get_archive_post_type() . '.css');
+    }
+
+    if ($criticalCss) {
+        $style = '<style type="text/css">';
+        $style .= $criticalCss;
+        $style .= '</style>';
+
+        echo $style;
+    }
+}
+
+function hardLoadCriticalCss()
+{
+
+    if (is_search()) {
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-blog.css');
+    }
+
+    if (is_single()) {
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-single-blog.css');
+    }
+
+    if (is_category() || is_tag()) {
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-blog.css');
+    }
+
+    if( is_page_template('templates/page-event-details-ny.php') ){
+        $criticalCss = file_get_contents(ASSETS_DIR . '/css/critical/critical-event-details.css');
+    }
+
+    if ($criticalCss) {
+        $style = '<style type="text/css">';
+        $style .= $criticalCss;
+        $style .= '</style>';
+
+        echo $style;
+    }
+
+}
+
