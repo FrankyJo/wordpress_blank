26ef7b2d2c349510e79e3e9c815228a2831bf4dd
Denis <denis.p@doubledmarketing.com>
2024-02-12 12:20:33 +0200

    edit style and want to add loadmore functional

--- customTheme/app/front/addStyles.php
+++ customTheme/app/front/addStyles.php
@@ -5,9 +5,9 @@ add_action('wp_footer', 'addFooterStyles');
 function addHeaderStyles()
 {
     if (is_front_page()) {
-        wp_enqueue_style('home_page', get_template_directory_uri() . '/assets/css/home.css');
+        wp_enqueue_style('home_page', get_template_directory_uri() . '/public/css/home.css');
     } else {
-        wp_enqueue_style('inner_page', get_template_directory_uri() . '/assets/css/inner.css');
+        wp_enqueue_style('inner_page', get_template_directory_uri() . '/public/css/inner.css');
     }
 }
 
